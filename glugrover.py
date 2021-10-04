7d57a6d47737299b4d0cfa87b06642b7413eff79
chloeskafte <chloe.skafte@yahoo.com>
2021-10-04 22:42:05 +1100

    line sensors

--- glugrover.py
+++ glugrover.py
@@ -1,8 +1,6 @@
 from gpiozero import Robot, LineSensor
 from time import sleep
 from signal import pause
-#Hi again, is this working?
-#Hi again, is this working?
 
 glug = Robot(left=(26,19), right=(15,18)) # Initialise motors
 left_Sensor = LineSensor(21) # Initialise left line sensor / Orange out cable
